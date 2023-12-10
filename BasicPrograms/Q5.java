347a2f5d1116c43384c835b30bed20bedb848d6f
Rupa-Rd <roopa.69.spr@gmail.com>
2023-12-10 21:59:19 +0530

    Q5 is added

---
+++ BasicPrograms/Q5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("A:");
+        int a = in.nextInt();
+        System.out.print("B:");
+        int b = in.nextInt();
+        if (a > b) {
+            System.out.println(a + " is Greater");
+        } else {
+            System.out.println(b + " is Greater");
+        }
+    }
+}
