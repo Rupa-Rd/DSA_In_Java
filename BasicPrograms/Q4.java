64fdcc9fa688662db053345a65333fc7fd91d734
Rupa-Rd <roopa.69.spr@gmail.com>
2023-12-10 21:24:10 +0530

    Q4 added

---
+++ BasicPrograms/Q4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("A:");
+        int a = in.nextInt();
+        System.out.print("B:");
+        int b = in.nextInt();
+        System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\nEnter choice:");
+        int choice = in.nextInt();
+        if (choice == 1) {
+            System.out.print(a + " + " + b + " = " + (a + b));
+        } else if (choice == 2) {
+            System.out.print(a + " - " + b + " = " + (a - b));
+        } else if (choice == 3) {
+            System.out.print(a + " * " + b + " = " + (a * b));
+        } else {
+            System.out.print(a + " / " + b + " = " + (a / b));
+        }
+    }
+}
